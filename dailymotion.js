ad90903a95b58b78aad4b4ed11cc8b6a25b05db3
luyong <luyong@nationalchip.com>
2018-09-07 16:21:03 +0800

    fix item total display error

--- dailymotion.js
+++ dailymotion.js
@@ -1,7 +1,8 @@
 
 var http = require('showtime/http');
+var channel_name_bak = '';
 
-var channel_list = ['music', 'videogames', 'sport', 'news', 'shortfilms', 'tv', 'fun', 
+var channel_list = ['music', 'videogames', 'sport', 'news', 'shortfilms', 'tv', 'fun',
     'lifestyle', 'auto', 'animals', 'creation', 'people', 'tech', 'travel', 'kids'];
 
 (function(plugin){  //插件入口
@@ -20,13 +21,20 @@ var channel_list = ['music', 'videogames', 'sport', 'news', 'shortfilms', 'tv', 
         }
     });
 
-//url = "https://api.dailymotion.com/channel/music/videos?fields=duration,id,owner.username,title,thumbnail_medium_url,views_total&sort=recent&family_filter=0&page=1&limit=4" 
+//url = "https://api.dailymotion.com/channel/music/videos?fields=duration,id,owner.username,title,thumbnail_medium_url,views_total&sort=recent&family_filter=0&page=1&limit=4"
 //{"page":1,"limit":1,"explicit":false,"total":8725281,"has_more":true,"list":[{"duration":90,"id":"x6bg27y","owner.username":"maciek970213","title":"stella no mahou ed","thumbnail_medium_url":"http:\/\/s2.dmcdn.net\/oi240\/160x120-jR-.jpg","views_total":0}]}
 
     plugin.addURI(PLUGIN_PREFIX + "channel_name:(.*)", function(page, channel_name){ //获取某个分组下的节目
 
+        var total = 0;
         var num_get_onetime = 16;
         var page_num = 1;
+
+        if(channel_name != channel_name_bak){
+            total = 0;
+            channel_name_bak = channel_name;
+        }
+
         function loader() {
             page_num++;
             var url = "https://api.dailymotion.com/channel/" + channel_name
@@ -64,18 +72,18 @@ var channel_list = ['music', 'videogames', 'sport', 'news', 'shortfilms', 'tv', 
                 };
 
                 //print("id: " + video_list[i].id + "\ttitle: " + video_list[i].title + "\ticon_url: " + video_list[i].thumbnail_medium_url);
-
+                total++;
                 var metadata = {
                     title: video_list[i].title,
                     icon: video_list[i].thumbnail_medium_url,
-                    extra_data: "total:" + response.total
+                    extra_data: "total dynamic:" + total
                 };
                 page.appendItem(PLUGIN_PREFIX + "video_info:" + JSON.stringify(video_info), "directory", metadata);
             }
 
             return true;
         }
-        
+
         loader();
         page.paginator = loader;
     });
@@ -84,13 +92,13 @@ var channel_list = ['music', 'videogames', 'sport', 'news', 'shortfilms', 'tv', 
     plugin.addURI(PLUGIN_PREFIX + "video_info:(.*)", function(page, video_info){  //多分辨节目
 
         var info = JSON.parse(video_info);
-        
+
         var hour =  Math.floor(info.duration / 3600);
         var min =  Math.floor((info.duration % 3600) / 60);
         var sec = info.duration % 60;
-        
+
         var duration_str = ((hour < 10)?("0"+ hour):(hour))
-                         + ((min < 10)?(":0"+ min):(":"+ min)) 
+                         + ((min < 10)?(":0"+ min):(":"+ min))
                          + ((sec < 10)?(":0"+ sec):(":"+ sec));
 
         var url = "http://www.dailymotion.com/embed/video/" + info.video_id;
@@ -104,7 +112,7 @@ var channel_list = ['music', 'videogames', 'sport', 'news', 'shortfilms', 'tv', 
             },
         }).toString();
         var strArray = response_text.match(/\{"type":"video\\\/mp4"[^\}]+\}/g);
-        
+
         for(var i in strArray){
             var play_url = JSON.parse(strArray[i]).url;
             var play_res = strArray[i].match(/\d+x\d+/i)[0];
@@ -122,7 +130,7 @@ var channel_list = ['music', 'videogames', 'sport', 'news', 'shortfilms', 'tv', 
                 icon: info.icon_url,
                 description: description_str
             };
-            
+
             page.appendItem(PLUGIN_PREFIX + "play_url:" + play_url, "video", metadata);
         }
     });
